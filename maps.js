602275ad32848fe256597307373b70860ef761bc
Colton <czpemberton@gmail.com>
2019-01-20 19:43:13 -0800

    Updates

--- maps.js
+++ maps.js
@@ -207,7 +207,7 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
     center: shop,
-    zoom: 16,
+    zoom: 15,
     streetViewControl: false,
     mapTypeControl: false,
     zoomControl: false,
